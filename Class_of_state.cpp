0f70789cc079618b7e4204f16dc4fb7e61f59720
NourSouid <42694199+NourSouid@users.noreply.github.com>
2021-02-24 16:00:34 +0100

    Delete Class_of_state.cpp

--- Class_of_state.cpp
+++
@@ -1,5 +0,0 @@
-#include "Class_of_state.h"
-//not used yet
- Class_Of_State operator-(Class_Of_State& a, Class_Of_State& b){
-            a.class_state -= (a.class_state)- (b.class_state) ;//other parameters of a do not change
-            return a;}//must be outside the class but do not compile otherwise
